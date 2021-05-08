d38fce9c24e03333341f9271047061b1843efc6e
Your Name <calebpm27@gmail.com>
2021-05-08 07:51:46 -0500

    Movimiento del personaje

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _rigidbody.velocity = input * 5;
+    }
+}
